7737d2000c9834ac95fb27fec08cb279b982e002
Aravind Divakaran <aravdiva@cisco.com>
2023-12-21 11:42:50 -0500

    Adding gitignore

--- src/nvmc/elasticbeanstalk/Helper.groovy
+++ src/nvmc/elasticbeanstalk/Helper.groovy
@@ -1,5 +1,16 @@
 package nvmc.elasticbeanstalk
 
+import com.amazonaws.auth.AWSCredentials
+import com.amazonaws.auth.AWSCredentialsProvider
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.regions.Region
+import com.amazonaws.services.elasticbeanstalk.AWSElasticBeanstalk
+import com.amazonaws.services.elasticbeanstalk.AWSElasticBeanstalkClientBuilder
+import com.amazonaws.services.elasticbeanstalk.model.*
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.services.s3.model.PutObjectRequest
 import groovy.util.logging.Slf4j
 
 @Slf4j
